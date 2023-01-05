bf201a8b24bdfa8de033e18fa96f875f476f38c5
booydar <ayd98@mail.ru>
2023-01-05 23:38:51 +0300

    fresh up + model selection + continuation

--- bot.py
+++ bot.py
@@ -1,19 +1,27 @@
 import os
 import re
 import datetime
+import json
+import random
 import telebot
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 from transcribe import ogg2wav, transcribe_audio
 from parse import parse_message
+from neural import Model
 
 
 class NoteBot(telebot.TeleBot):
     def __init__(self, cred_path='creds.txt'):
-        with open('creds.txt', 'r') as f:
-            API_TOKEN = f.read().strip()
-        super().__init__(API_TOKEN)
+        with open('config.json', 'r') as f:
+            d = json.load(f)
+            api_token = d['token']
+            self.db_path = d['db_path']
+            self.correct_chat_id = d['chat_id']
+        super().__init__(api_token)
         self.lang = 'ru-RU'
         self.tags = []
+        self.model = Model()
+        self.wait_value = False
 
 
     def transcribe_message(self, message):
@@ -30,10 +38,10 @@ class NoteBot(telebot.TeleBot):
         return transcription
 
     
-    def save_last_message(self, sv_folder='saved_notes'):
+    def save_last_message(self):
         dt = str(datetime.datetime.now())
         dt_pfx = re.sub(r'[:]', '-', dt.split('.')[0])
-        sv_path = f'{sv_folder}/{dt_pfx}.md'
+        sv_path = os.path.join(self.db_path, f'{dt_pfx}.md')
         
         note = parse_message(self.last_message, self.tags)
         with open(sv_path, 'w') as f:
@@ -53,9 +61,10 @@ def lang_markup():
 
 def save_markup():
     markup = InlineKeyboardMarkup()
-    markup.row_width = 2
+    markup.row_width = 3
     markup.add(InlineKeyboardButton("Сохранить", callback_data='save'),
-                InlineKeyboardButton("Добавить тэг", callback_data='hashtag'))
+                InlineKeyboardButton("Добавить тэг", callback_data='hashtag'),
+                InlineKeyboardButton("Продолжить", callback_data='continue'))
     return markup
 
 
@@ -68,11 +77,20 @@ def callback_query(call):
         bot.answer_callback_query(call.id, "English")
         bot.lang = call.data
     elif call.data == 'save':
-        bot.save_last_message()     
-        bot.answer_callback_query(call.id, "Note saved")
+        # print('chat id', bot.chat_id)
+        if str(bot.chat_id) == str(bot.correct_chat_id):
+            bot.save_last_message()     
+            bot.answer_callback_query(call.id, "Note saved")
+        else:
+            bot.send_message(bot.chat_id, "Ты не Айдар, не буду ничего сохранять!")
+            bot.send_message(bot.correct_chat_id, f"{bot.chat_id} пытается сохранить тебе заметку!")
     elif call.data == 'hashtag':
         bot.answer_callback_query(call.id)
+        bot.wait_value = 'tag'
         bot.send_message(bot.chat_id, "Введи название тега")
+    elif call.data == 'continue':
+        text = bot.model.generate(bot.last_message)
+        bot.send_message(bot.chat_id, text)
 
 
 @bot.message_handler(commands=['start'])
@@ -90,7 +108,32 @@ def handle_voice(message):
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    bot.tags.append(message.text)
+    bot.chat_id = message.chat.id
+    if message.text.startswith('/random_number'):
+        bot.send_message(message.chat.id, random.randint(0, 100))
+    elif message.text.startswith('/yes_or_no'):
+        bot.send_message(message.chat.id, random.choice(('yes', 'no')))
+    elif message.text.startswith('/language'):
+        bot.send_message(message.chat.id, 'Выбери язык:', reply_markup=lang_markup())
+    elif message.text.startswith('/set_min_gen_length'):
+        bot.send_message(message.chat.id, 'to what value?')
+        bot.wait_value = 'min_length'
+    elif message.text.startswith('/set_max_gen_length'):
+        bot.send_message(message.chat.id, 'to what value?')
+        bot.wait_value = 'max_length'
+    elif message.text.startswith('/set_num_beams'):
+        bot.send_message(message.chat.id, 'to what value?')
+        bot.wait_value = 'num_beams'
+    elif bot.wait_value == 'tag':
+        bot.tags.append(message.text)
+        bot.wait_value = False
+    elif bot.wait_value:
+        bot.model.config[bot.wait_value] = int(message.text)
+        bot.wait_value = False
+    else:
+        bot.last_message = message.text
+        bot.send_message(message.chat.id, message.text, reply_markup=save_markup())
+        
     
 
 bot.infinity_polling()
---
+++ neural.py
@@ -0,0 +1,39 @@
+from transformers import GPT2LMHeadModel, GPTNeoForCausalLM, GPT2Tokenizer
+
+CONFIG = {
+        'model_name': 'sberbank-ai/mGPT',
+        'model_cls': GPT2LMHeadModel,
+        # 'model_name': 'EleutherAI/gpt-neo-2.7B',
+        # 'model_cls': GPTNeoForCausalLM,
+        'min_length': 20,
+        'max_length': 300,
+        'num_beams': 1,
+        'num_return_sequences': 1,
+        'no_repeat_ngram_size': 2,
+        'device': 'cuda'}
+        
+class Model:
+    def __init__(self, config=CONFIG):
+        gpt2_name = config['model_name']
+        self.model = config['model_cls'].from_pretrained(gpt2_name)
+        self.tokenizer = GPT2Tokenizer.from_pretrained(gpt2_name)
+        self.model.to(config['device'])
+        self.config = config
+
+
+    def generate(self, text):
+        input_ids = self.tokenizer.encode(text, return_tensors='pt').to(self.config['device'])
+        num_tokens = input_ids.shape[1]
+
+        # set return_num_sequences > 1
+        beam_outputs = self.model.generate(
+            input_ids, 
+            min_length=num_tokens + self.config['min_length'],
+            max_length=num_tokens + self.config['max_length'], 
+            num_beams=self.config['num_beams'], 
+            no_repeat_ngram_size=self.config['no_repeat_ngram_size'], 
+            num_return_sequences=self.config['num_return_sequences'], 
+            early_stopping=True
+        )
+
+        return [self.tokenizer.decode(b, skip_special_tokens=True) for b in beam_outputs]
